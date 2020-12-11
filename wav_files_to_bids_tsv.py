b03d99f997a5f70d43f10b8f8b09f959428c67f6
jannenold <73650127+jannenold@users.noreply.github.com>
2020-12-11 12:20:18 +0100

    Update wav_files_to_bids_tsv.py

--- wav_files_to_bids_tsv.py
+++ wav_files_to_bids_tsv.py
@@ -5,23 +5,116 @@ import json
 import os
 import librosa as lbr
 
-def your_own_feature_extractor(filename,  **kwargs):
-    '''Template for your own feature extractor.
-    
-    It needs to process a filename and return three variables:
-    an ndarray of features of shape (samples, features), 
-    the sampling rate of the features in Hz,
-    names for the columns of each features
+def mps_extract(filename, sr = 44100, n_fft = 512, hop_length = 512, mps_n_fft = 500, 
+                mps_hop_length = 500, plot_mps = False, **kwargs)
 
+               
+'''                
     Parameters
     ----------
-    filename : str, path to wav file to be converted
+
+filename:       str, path to wav files to be converted
+sr:             int, sampling rate for wav file (Default: 44100 Hz)
+n_fft:          int, window size for mel spectrogram extraction (Default: 512)
+hop_length:     int, step size for mel spectrogram extraction (Default: 512)
+mps_n_fft:      int, window size for mps extraction (Default: 500)
+mps_hop_length: int, step size for mps extraction (Default: 500)
+plot_mps:       bool, if true the Mel spectrogram for the first window and according mps will be plotted (Default: False)
+kwargs:         additional keyword arguments that will be transferred to librosa's melspectrogram function
 
     Returns
     -------
-    tuple of a feature representation, the repetition time in seconds, and the names of all features
-    '''
-    pass
+
+tuple of a feature representation (2-dimensional array: samples x feature)
+repitition time in seconds
+names of all features (list of strings of mod/s for each mod/Hz)        
+
+'''
+
+
+wav, _ = lbr.load(filename, sr=sr) 
+
+mel_spec = lbr.feature.melspectrogram(y=wav, sr=sr, hop_length=hop_length,
+                                              **kwargs)
+mel_spec = mel_spec.T
+
+if mps_n_fft >= mel_spec.shape[0]:
+    raise ValueError("The mps window size exceeds the Mel spectrogram. Please enter a smaller integer.")
+
+if mps_hop_length >= mel_spec.shape[0]:
+    raise ValueError("The mps step size exceeds the Mel spectrogram. Please enter a smaller integer.")
+
+mps_all = []
+mps_plot = []
+nyquist_mps = int(np.ceil(mel_spec.shape[1]/2))
+
+for i in range(1,101):
+    mps = np.fft.fft2(mel_spec[mps_n_fft*(i-1):mps_n_fft*i,:])
+    mps = np.abs(np.fft.fftshift(mps))
+    mps_plot.append(mps)
+    mps = np.reshape(mps,(1,np.size(mps)))
+    mps_all.append(mps)
+    
+mps_all = np.array(mps_all)
+mps_plot = np.array(mps_plot)
+mps_all = np.concatenate(mps_all)
+    
+fs_spectrogram = sr/hop_length
+fs_mps = fs_spectrogram/mps_hop_length
+
+
+freq_step_log = np.log(mel_spec[1,:])
+freq_step_log = freq_step_log[1] - freq_step_log[0]
+
+
+mps_freqs = np.fft.fftshift(np.fft.fftfreq(mel_spec.shape[1], d = freq_step_log)) 
+mps_times = np.fft.fftshift(np.fft.fftfreq(mps_n_fft, d = 1. /fs_spectrogram)) 
+
+if plot_mps = True: 
+    fig, (ax1,ax2)= plt.subplots(1, 2, figsize=(20, 10))
+
+    first_mel = mel_spec[0:mps_n_fft,:]
+    time = np.arange(0,mps_n_fft)*fs_spectrogram
+    frequency = np.arange(0,mel_spec.shape[1])*fs_mps
+
+    image1 = ax1.imshow(first_mel.T, origin = 'lower', aspect = 'auto')
+    ax1.set_xticks(np.arange(0,mps_n_fft,20))
+    ax1.set_yticks(np.arange(0,first_mel.shape[1],10))
+    x1 = ax1.get_xticks()
+    y1 = ax1.get_yticks()
+    ax1.set_xticklabels(['{:.0f}'.format(xtick) for xtick in time[x1]])
+    ax1.set_yticklabels(['{:.2f}'.format(ytick) for ytick in frequency[y1]])
+    ax1.set_title('Mel Spectrogram 1st window')
+    ax1.set_ylabel('Frequencyband (Hz)')
+    ax1.set_xlabel('Time (s)')
+    cbar = fig.colorbar(image1, ax = ax1, format='%+2.0f dB')
+    cbar.set_label('dB')
+    
+    image2 = ax2.imshow(np.log(mps_plot[0,:,nyquist_mps:].T), origin = 'lower', aspect = 'auto')
+    mps_freqs2 = mps_freqs[nyquist_mps:,]
+    ax2.set_xticks(np.arange(0,len(mps_times),20))
+    ax2.set_yticks(np.arange(0,len(mps_freqs2),8))
+    x2 = ax2.get_xticks()
+    y2 = ax2.get_yticks()
+    ax2.set_xticklabels(['{:.0f}'.format(xtick2) for xtick2 in mps_times[x2]])
+    ax2.set_yticklabels(['{:.2f}'.format(ytick2) for ytick2 in mps_freqs2[y2]])
+    ax2.set_title(' MPS for Mel Spectrogram (1st window)')
+    ax2.set_xlabel('Temporal Modulation (mod/s)')
+    ax2.set_ylabel('Spectral Modulation (cyc/oct)')
+    cbar = fig.colorbar(image2, ax=ax2)
+    cbar.set_label('(log) MPS')
+
+
+    
+# Extracting feature names                     
+names_features = ['{0:.2f} mod/s {1:.2f} cyc/oct)'.format(mps_time, mps_freq) 
+                  for mps_time in mps_times for mps_freq in mps_freqs]
+
+# Determine MPS repitition time 
+mps_rep_time = 1/fs_mps
+                                
+return mps_all, mps_rep_time, names_features
+
 
 def get_mel_spectrogram(filename, log=True, sr=44100, hop_length=512, **kwargs):
     '''Returns the (log) Mel spectrogram of a given wav file, the sampling rate of that spectrogram and names of the frequencies in the Mel spectrogram
